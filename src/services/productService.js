8f89271d5cae53bc16a1db8cd2004b17c1eeba68
jessicafranklindr <jeh_f@live.com>
2021-10-02 17:31:17 -0300

    product service updated

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,9 +1,15 @@
 import axios from 'axios'
-const baseURL = "/api/"
+const baseURL = "http://localhost:3001/api/"
 
 
 const getAll = () => {
     return axios.get(baseURL + "products")
                 .then(response => response.data)
 }
+
+
+const productService = {getAll}
+
+export default productService;
+
 

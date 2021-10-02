8c5bc875176616a3698a9b75af20d7e37551fe1b
jessicafranklindr <jeh_f@live.com>
2021-10-02 15:27:41 -0300

    react-router-dom installed and working with navbar

--- src/App.js
+++ src/App.js
@@ -1,19 +1,48 @@
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch, Route, Link
+} from "react-router-dom";
+import productDisplay from './ListOfProducts';
 
 function App() {
+
+
+
   return (
+   <Router>
     <div className="App">
       <ul className="Navbar">
-        <li><a href='#'>Home</a></li>
-        <li><a href='#'>Menu</a></li>
-        <li><a href='#'>Contact</a></li>
+        <li><Link to= "/">Home</Link></li>
+        <li><Link to= "/Menu">Menu</Link></li>
+        <li><Link to= "/Contact-us">Contact</Link></li>
         <li><img src="" className="App-logo" alt="logo" /> </li>
-        <li><a href='#'>FAQ</a></li>
-        <li><a href='#'>Cart</a></li>
-        <li><a href='#'>Login</a></li>
+        <li><Link to= "/FAQ">FAQ</Link></li>
+        <li><Link to= "/My-cart">Cart</Link></li>
+        <li><Link to= "/Login">Login</Link></li>
       </ul>
+      <Switch>
+        <Route path="/products/:id">
+          
+        </Route>
+        <Route path="/Menu">
+          
+        </Route>
+        <Route path="/Contact-us">
+
+        </Route>
+        <Route path="/FAQ">
+
+        </Route>
+        <Route path="/My-cart">
+
+        </Route>
+        <Route path="/Login">
+
+        </Route>
+      </Switch>
     </div>
-  );
+ </Router>  );
 }
 
 export default App;

b399b09dcb5a37e6836b3c31d5844038b0de4ab1
Mariel Uykim <marielanne.uykim@students.mq.edu.au>
2021-10-04 20:15:37 +1100

    added location and social media section

--- src/pages/aboutScreen.js
+++ src/pages/aboutScreen.js
@@ -3,8 +3,11 @@ import '../AboutScreen.css'
 import {
     Link
  } from "react-router-dom";
- import logo from '../assets/j-logo.png'
-
+import logo from '../assets/j-logo.png'
+import fbIcon from '../assets/facebook.png'
+import igIcon from '../assets/instagram.png'
+import emailIcon from '../assets/email.png'
+import twitterIcon from '../assets/twitter.png'
 const About = () => {
     return(
         <div className="aboutPage">
@@ -31,7 +34,33 @@ const About = () => {
                     eleifend eget sapien ac aliquet. Donec vehicula auctor lacus, ac maximus sapien 
                     mattis congue. Donec tortor diam.</p>
             </div>
+            
+            <div className = "locationSection">
+                <h2>LOCATION</h2>
+                <div className = "googleMap">
+                <iframe
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6680.80187835327!2d151.1085164407265!3d-33.77697748024791!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6b12a60cc14b5cfb%3A0x500fcb6f9f97861b!2sMacquarie%20Centre!5e0!3m2!1sen!2sph!4v1633336204159!5m2!1sen!2sph"
+                width="700"
+                height="450"
+                frameBorder="0"
+                style={{ border: 0 }}
+                allowFullScreen=""
+                aria-hidden="false"
+                tabIndex="0"
+                />
+                </div>
+                <h6>Loren ipsum dolor sit amet</h6>
+            </div>
 
+            <div className="socialMedia">
+                <h2>KEEP IN TOUCH</h2>
+                <ul className="sm-icons">
+                    <li className="sm-icon"><a href="#"><img src={emailIcon} width="80px" height="80px" alt="email"/></a></li>
+                    <li className="sm-icon"><a href="#"><img src={igIcon} width="80px" height="80px" alt="instagram"/></a></li>
+                    <li className="sm-icon"><a href="#"><img src={twitterIcon} width="80px" height="80px" alt="twitter"/></a></li>
+                    <li className="sm-icon"><a href="#"><img src={fbIcon} width="80px" height="80px" alt="facebook"/></a></li>
+                </ul>
+            </div>
         </div>
     ) 
 }

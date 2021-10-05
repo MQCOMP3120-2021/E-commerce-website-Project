f774a633ea1b16808f4191d29e122de404bbab1f
Mariel Uykim <marielanne.uykim@students.mq.edu.au>
2021-10-05 22:17:57 +1100

    design for review section of product

--- src/pages/Product.js
+++ src/pages/Product.js
@@ -35,6 +35,24 @@ const Product = ({product, moreCart}) => {
                     </div>
                 </div>
             </div>
+            <div className="reviewSec">
+                <h3>Reviews</h3>
+                <hr/>
+                <div className="productReviews">
+                    <div className="productReview">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <h5>USERNAME</h5>
+                    </div>
+                    <div className="productReview">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <h5>USERNAME</h5>
+                    </div>
+                    <div className="productReview">
+                        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+                        <h5>USERNAME</h5>
+                    </div>
+                </div>
+            </div>
         </>
     )
 }

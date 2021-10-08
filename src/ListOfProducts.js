1db69aadf918bfddc92eddca82e58ce3fbb1db6e
jessicafranklindr <jeh_f@live.com>
2021-10-08 12:16:26 -0300

    Single product view working

--- src/ListOfProducts.js
+++ src/ListOfProducts.js
@@ -1,6 +1,10 @@
 import React from 'react'
 import { useParams, Link
 } from "react-router-dom"
+//import './Product.css'
+import defaultImg from './assets/defaultImg.jpg'
+import logo from './assets/logo-black.png'
+
 
 const ListOfProducts = ({products}) => {
      return(
@@ -19,24 +23,62 @@ const ListOfProducts = ({products}) => {
 }
 
 const SingleProduct = ({product, moreCart}) => {
-     const id = Number(useParams().id)
+    const id = Number(useParams().id)
     const singleP = product.find(p=> p.id === id)
     console.log(singleP)
     if(singleP){
         return(
-            <main>
-            <img src={product.photo} alt="bread"></img>
-            <h3>{product.name}</h3>
-            <p>{product.description}</p>
-            <p>{product.price}</p>
-            <button >Quantity button goes here</button>
-            <button onClick={() => moreCart(product)}>Add to Cart button goes here</button>
-
-            <ul>
-              <li>Review goes here</li>
+            <>
+            <ul className="Navbar">
+                    <li><Link to= "/" className="brightNav">Home</Link></li>
+                    <li><Link to= "/Menu" className="brightNav">Menu</Link></li>
+                    <li><Link to= "/About" className="brightNav">About</Link></li>
+                    <li><img src={logo} className="App-logo" alt="logo" height="100px" width="100px"/> </li>
+                    <li><Link to= "/FAQ" className="brightNav">FAQ</Link></li>
+                    <li><Link to= "/My-cart" className="brightNav">Cart</Link></li>
+                    <li><Link to= "/Login" className="brightNav">Login</Link></li>
             </ul>
-            </main>
-        )
+            {product ? (
+                <>
+                    <div className="productCard">
+                        <img src={product.photo} className="productImg" onError={()=>this.img.src=defaultImg}/>
+                        <div className="productInfo">
+                            <h3>{product.name}</h3>
+                            <p>{product.description}</p>
+                            <h6>{product.price}</h6>
+                            <div className="quantityBtn">
+                                <input type="button" value=" - " className="decrement"/>
+                                <input type="number" value="1" min="1" className="qty"/>
+                                <input type="button" value=" + " className="incriment"/>
+                            </div>
+                            <div ClassName="submitBtn">
+                                <button type="submit">ADD TO CART</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="reviewSec">
+                        <h3>Reviews</h3>
+                        <hr/>
+                        {/** FIX THIS FOR REVIEWS NOT WORKING PROPERLY (error: CANT MAP FROM REVIEWS)
+                        <div className="productReviews">
+                            {(product.reviews).map((review) => {
+                                return <div className="productReview">
+                                            <p>{review}</p>
+                                            <h5>USERNAME</h5>
+                                        </div>
+                            })}
+                        </div>*/}
+                    </div>
+                </>
+            ) : (
+                <>
+                    <div className="product404">
+                        <h3>Product not found :(</h3>
+                    </div>
+                </>
+            )}
+        </>
+    )
     }
 }
 
